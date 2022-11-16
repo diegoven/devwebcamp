fd457baae0ad0cff1ea813ba62f171423cf6b433
Diego Suárez <diego.suarezg96@gmail.com>
2022-11-15 23:01:36 -0500

    initial upload of files provided by the instructor

---
+++ includes/funciones.php
@@ -0,0 +1,14 @@
+<?php
+
+function debuguear($variable): string
+{
+    echo "<pre>";
+    var_dump($variable);
+    echo "</pre>";
+    exit;
+}
+function s($html): string
+{
+    $s = htmlspecialchars($html);
+    return $s;
+}

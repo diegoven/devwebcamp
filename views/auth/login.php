c16829fc630442162bb13b25dadf33dcae425994
Diego Suárez <diego.suarezg96@gmail.com>
2022-11-16 22:35:36 -0500

    Adds authentication of registered users

--- views/auth/login.php
+++ views/auth/login.php
@@ -2,8 +2,12 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Inicia sesión en DevWebCamp</p>
 
-    <form action="" class="formulario">
-    <div class="formulario__campo">
+    <?php
+    require_once __DIR__ . "../../templates/alertas.php";
+    ?>
+
+    <form method="POST" action="/login" class="formulario">
+        <div class="formulario__campo">
             <label for="email" class="formulario__label">E-mail </label>
             <input type="email" name="email" id="email" class="formulario__input" placeholder="correo@correo.com">
         </div>
@@ -19,5 +23,5 @@
     <div class="acciones">
         <a href="/registro" class="acciones__enlace">¿Aún no tienes cuenta? ¡Obtener una!</a>
         <a href="/olvide" class="acciones__enlace">¿Olvidaste tu contraseña?</a>
-        </div>
+    </div>
 </main>
